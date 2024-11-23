52e1cc42dba2fd8a26d5038a306976fb846cb532
lalith <docs.work.gooogle@gmail.com>
2024-11-23 14:54:23 +0530

    Controlled Responses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,16 +14,64 @@ public class Main {
             serverSocket = new ServerSocket(PORT);
             while (true) {
                 Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
-                System.out.println("accepted new connection");
-                clientSocket
-                    .getOutputStream()
-                    .write(
+                System.out.println("New Client connection Accepted.");
+
+                String requestLine = null;
+
+                //input-stream
+                InputStream ipStream = clientSocket.getInputStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = ipStream.read(buffer);
+                if (bytesRead != -1) {
+                    String received = new String(buffer, 0, bytesRead);
+                    int requestEOL = received.indexOf("\r\n");
+                    if (requestEOL != -1) {
+                        requestLine = received.substring(0, requestEOL).trim();
+                    }
+                    System.out.println(received);
+                }
+                if (requestLine == null) {
+                    System.out.println("Invalid request.");
+                    clientSocket.close();
+                    continue;
+                }
+                String[] requestParts = requestLine.split(" ");
+                if (requestParts.length < 3) {
+                    System.out.println("Malformed request.");
+                    clientSocket.close();
+                    continue;
+                }
+
+                String requestTarget = requestParts[1];
+                System.out.println("Request target: " + requestTarget);
+
+                //output-stream
+                OutputStream opstream = clientSocket.getOutputStream();
+                if ("/".equals(requestTarget)) {
+                    String responseBody = "Hello from server!";
+                    opstream.write(
                         ("HTTP/1.1 200 OK\r\n" +
                             "Content-Type: text/plain\r\n" +
-                            "Content-Length: 20\r\n" +
+                            "Content-Length:" +
+                            responseBody.length() +
                             "\r\n" +
-                            "Hello from the server!").getBytes()
+                            "\r\n" +
+                            responseBody).getBytes()
                     );
+                    opstream.flush();
+                } else {
+                    String responseBody = "404 Not Found";
+                    opstream.write(
+                        ("HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "Content-Length: " +
+                            responseBody.length() +
+                            "\r\n" +
+                            "\r\n" +
+                            responseBody).getBytes()
+                    );
+                    opstream.flush();
+                }
                 clientSocket.close();
             }
         } catch (IOException e) {
